4026e3941b9a462190d28377cca13e31d94e1719
Racha <9rachadev@gmail.com>
2022-02-10 21:11:12 +0100

    Comentarios

--- Assets/Scripts/NewBehaviourScript.cs
+++ Assets/Scripts/NewBehaviourScript.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
+    //Array o List con 10 colores
+
+    //Repasar Golden Path Module Foundation y Module One
+
     // Start is called before the first frame update
     void Start()
     {
